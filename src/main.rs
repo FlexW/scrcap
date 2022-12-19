dc07be764613406e7aecf16070686113874c4c6a
Felix Weilbach <felix.weilbach@t-online.de>
2022-12-19 21:14:03 +0100

    Display Wayland globals

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use wayland_client::{Display, GlobalManager};
+
 fn main() {
-    println!("Hello, world!");
+    // Connect to the server
+    let display = Display::connect_to_env().unwrap();
+
+    let mut event_queue = display.create_event_queue();
+
+    let attached_display = (*display).clone().attach(event_queue.token());
+
+    // We use the GlobalManager convenience provided by the crate, it covers
+    // most classic use cases and avoids us the trouble to manually implement
+    // the registry
+    let globals = GlobalManager::new(&attached_display);
+
+    // A roundtrip synchronization to make sure the server received our registry
+    // creation and sent us the global list
+    event_queue.sync_roundtrip(&mut (), |_, _, _| unreachable!()).unwrap();
+
+    // Print the list
+    for (id, interface, version) in globals.list() {
+        println!("{}: {} (version {})", id, interface, version);
+    }
 }
